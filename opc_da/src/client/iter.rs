41f112a7483809b77a0c8303fb8bda5263e65075
ronbb <ronbiaobiao@gmail.com>
2024-12-03 11:21:58 +0800

    Enhance GuidIter with caching and error handling; add COM initialization in Client

--- opc_da/src/client/iter.rs
+++ opc_da/src/client/iter.rs
@@ -1,18 +1,59 @@
-pub struct GuidIter(pub(super) windows::Win32::System::Com::IEnumGUID);
+/// Iterator over COM GUIDs from IEnumGUID.  
+///
+/// # Safety  
+/// This struct wraps a COM interface and must be used according to COM rules.  
+pub struct GuidIter {
+    iter: windows::Win32::System::Com::IEnumGUID,
+    cache: [windows_core::GUID; 16],
+    count: u32,
+    finished: bool,
+}
+
+impl GuidIter {
+    /// Creates a new iterator from a COM interface.  
+    pub(super) fn new(iter: windows::Win32::System::Com::IEnumGUID) -> Self {
+        Self {
+            iter,
+            cache: [windows_core::GUID::zeroed(); 16],
+            count: 0,
+            finished: false,
+        }
+    }
+}
 
 impl Iterator for GuidIter {
-    type Item = windows_core::GUID;
+    type Item = windows_core::Result<windows_core::GUID>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut ids = [windows_core::GUID::zeroed(); 1];
-        let mut count = ids.len() as u32;
+        if self.finished {
+            return None;
+        }
 
-        unsafe { self.0.Next(&mut ids, Some(&mut count)).ok() }.ok()?;
+        if self.count == 0 {
+            if self.count > 0 {
+                self.count -= 1;
+                return Some(Ok(self.cache[self.count as usize]));
+            }
 
-        if count == 0 {
+            let ids = &mut self.cache;
+            let count = &mut self.count;
+
+            let code = unsafe { self.iter.Next(ids, Some(count)) };
+            if code.is_err() {
+                self.finished = true;
+                return Some(Err(windows_core::Error::new(
+                    code,
+                    "Failed to get next GUID",
+                )));
+            }
+        }
+
+        if self.count == 0 {
+            self.finished = true;
             None
         } else {
-            Some(ids[0])
+            self.count -= 1;
+            Some(Ok(self.cache[self.count as usize]))
         }
     }
 }
--- opc_da/src/client/mod.rs
+++ opc_da/src/client/mod.rs
@@ -7,14 +7,22 @@ pub use iter::*;
 pub struct Client {}
 
 impl Client {
-    pub fn get_servers(filter: ServerFilter) -> windows_core::Result<GuidIter> {
+    pub fn ensure_com() -> windows_core::Result<()> {
+        static COM_INIT: std::sync::Once = std::sync::Once::new();
+        static mut COM_RESULT: Option<windows_core::HRESULT> = None;
+
         unsafe {
-            windows::Win32::System::Com::CoInitializeEx(
-                None,
-                windows::Win32::System::Com::COINIT_MULTITHREADED,
-            )
-            .ok()?;
-        };
+            COM_INIT.call_once(|| {
+                COM_RESULT = Some(windows::Win32::System::Com::CoInitializeEx(
+                    None,
+                    windows::Win32::System::Com::COINIT_MULTITHREADED,
+                ));
+            });
+            COM_RESULT.unwrap_or(windows::Win32::Foundation::S_OK).ok()
+        }
+    }
+
+    pub fn get_servers(filter: ServerFilter) -> windows_core::Result<GuidIter> {
         let id = unsafe {
             windows::Win32::System::Com::CLSIDFromProgID(windows_core::w!("OPC.ServerList.1"))?
         };
@@ -30,20 +38,32 @@ impl Client {
         };
 
         let iter = unsafe {
-            servers.EnumClassesOfCategories(
-                &filter
-                    .available_versions
-                    .iter()
-                    .map(|v| v.to_guid())
-                    .collect::<Vec<_>>(),
-                &filter
-                    .requires_versions
-                    .iter()
-                    .map(|v| v.to_guid())
-                    .collect::<Vec<_>>(),
-            )?
+            servers
+                .EnumClassesOfCategories(
+                    &filter
+                        .available_versions
+                        .iter()
+                        .map(|v| v.to_guid())
+                        .collect::<Vec<_>>(),
+                    &filter
+                        .requires_versions
+                        .iter()
+                        .map(|v| v.to_guid())
+                        .collect::<Vec<_>>(),
+                )
+                .map_err(|e| {
+                    windows_core::Error::new(e.code(), "Failed to enumerate server classes")
+                })?
         };
 
-        Ok(GuidIter(iter))
+        Ok(GuidIter::new(iter))
+    }
+}
+
+impl Drop for Client {
+    fn drop(&mut self) {
+        unsafe {
+            windows::Win32::System::Com::CoUninitialize();
+        }
     }
 }
